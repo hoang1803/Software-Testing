8872aa3795305cd11eda3569573858f4b86e2c64
hoang1803 <100293832+hoang1803@users.noreply.github.com>
2023-09-28 23:17:23 +0700

    Update water.cpp

--- Kiểm thử chức năng/water.cpp
+++ Kiểm thử chức năng/water.cpp
@@ -5,30 +5,51 @@ using namespace std;
 int a[] = {0, 10, 20, 30, INT_MAX};
 int price[] = {6000, 7000, 9000, 16000};
 
-void getAmount(int &water) {
+void getAmount(int &water)
+{
     cin >> water;
 }
 
-void getPrice(const int &water) {
-    if (water < 0) {
+void getPrice(const int &water)
+{
+    if (water < 0)
+    {
         cout << "Invalid Input";
-        return ;
+        return;
     }
-    
+
     int totalPrice = 0;
-    for (int i = 0; i < 4; i++) {
-        if (water >= a[i]) {
-            totalPrice += (min(water, a[i + 1]) - a[i]) * price[i];
-        }
-        else {
-            break ;
-        }
+    // for (int i = 0; i < 4; i++) {
+    //     if (water >= a[i]) {
+    //         totalPrice += (min(water, a[i + 1]) - a[i]) * price[i];
+    //     }
+    //     else {
+    //         break ;
+    //     }
+    // }
+
+    if (water <= 10)
+    {
+        totalPrice = water * price[0];
     }
-    
+    else if (water <= 20)
+    {
+        totalPrice = (water - 10) * price[1] + 10 * price[0];
+    }
+    else if (water <= 30)
+    {
+        totalPrice = (water - 20) * price[2] + 10 * (price[1] + price[0]);
+    }
+    else
+    {
+        totalPrice = (water - 30) * price[3] + 10 * (price[2] + price[1] + price[0]);
+    }
+
     cout << totalPrice;
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     // File chứa Input
